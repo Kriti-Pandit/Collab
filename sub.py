cfff685852e5770d848aac44730339736bb17abc
preranamshirur <148961018+preranamshirur@users.noreply.github.com>
2025-02-03 15:59:58 +0530

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Subtracting the two numbers
+difference = num1 - num2
+
+# Displaying the result
+print("The difference is:", difference)
