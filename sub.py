6c589a46117fd1bee3f4786878eb9e2b4b4c23c4
Kriti-Pandit <155658101+Kriti-Pandit@users.noreply.github.com>
2025-02-03 16:31:09 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,9 +1,5 @@
-# Taking input from the user
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-# Subtracting the two numbers
-difference = num1 - num2
+difference = 5 - 2
 
 # Displaying the result
 print("The difference is:", difference)
