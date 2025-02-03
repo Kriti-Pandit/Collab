9c762453502ab7d895f13a87e64885231cffa1aa
Nidhiks767 <155746052+Nidhiks767@users.noreply.github.com>
2025-02-03 16:00:53 +0530

    Create add2.py

---
+++ add2.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding the two numbers
+sum_result = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum_result)
