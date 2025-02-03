12d67e14cc7af141c661e273c87e7624992c5e27
Kriti-Pandit <155658101+Kriti-Pandit@users.noreply.github.com>
2025-02-03 16:30:31 +0530

    Update add2.py

--- add2.py
+++ add2.py
@@ -1,9 +1,5 @@
-# Taking input from the user
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-# Adding the two numbers
-sum_result = num1 + num2
+sum_result = 3 + 5
 
 # Displaying the result
 print("The sum is:", sum_result)
